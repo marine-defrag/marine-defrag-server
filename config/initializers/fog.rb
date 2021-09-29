f658499926615354b8fa23c89ea682b7506c64d1
Philip Arndt <git@p.arndt.io>
2021-09-30 09:33:07 +1300

    Set up CI with GitHub Actions

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,6 +1,8 @@
-::FogStorage = Fog::Storage.new(
-  provider: 'AWS',
-  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  region: ENV['AWS_BUCKET_REGION']
-)
+if ENV["AWS_ACCESS_KEY_ID"].present? && ENV["AWS_SECRET_ACCESS_KEY"].present?
+  ::FogStorage = Fog::Storage.new(
+    provider: "AWS",
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: ENV["AWS_BUCKET_REGION"]
+  )
+end
