58345ad61194b2e96bfa16efc47472806aae3fe3
Philip Arndt <git@p.arndt.io>
2021-11-01 21:10:58 +1300

    It's good, but unrelated to the PR

--- app/policies/framework_taxonomy_policy.rb
+++ app/policies/framework_taxonomy_policy.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
-class FrameworkTaxonomyPolicy < SystemPolicy
+class FrameworkTaxonomyPolicy < ApplicationPolicy
   def permitted_attributes
-    [:framework_id, :taxonomy_id]
+    [:framework_id,
+      :taxonomy_id]
+  end
+
+  def update?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def destroy?
+    false
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
   end
 end
