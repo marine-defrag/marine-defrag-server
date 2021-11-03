c2b33f4cfd061a278d1d7e186d1ef66d6aa654ef
Philip Arndt <git@p.arndt.io>
2021-11-03 15:44:36 +1300

    rename _type things to type

--- db/migrate/20211021075234_create_measure_types.rb
+++ db/migrate/20211021075234_create_measure_types.rb
@@ -1,4 +1,4 @@
-class CreateMeasureTypes < ActiveRecord::Migration[6.1]
+class CreateMeasuretypes < ActiveRecord::Migration[6.1]
   def change
     create_table :measure_types do |t|
       t.string :title, null: false
