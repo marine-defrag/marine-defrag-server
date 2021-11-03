a1b2885abf979b34eb609744078492e1c374965d
Philip Arndt <git@p.arndt.io>
2021-11-03 16:19:46 +1300

    Update for changes from #61

--- db/migrate/20211103070810_create_actortypes_taxonomies.rb
+++ db/migrate/20211103070810_create_actortypes_taxonomies.rb
@@ -1,7 +1,7 @@
 class CreateActortypesTaxonomies < ActiveRecord::Migration[6.1]
   def change
     create_table :actortypes_taxonomies do |t|
-      t.belongs_to :actor_type, foreign_key: true, index: true, null: false
+      t.belongs_to :actortype, foreign_key: true, index: true, null: false
       t.belongs_to :taxonomy, foreign_key: true, index: true, null: false
 
       t.timestamps
