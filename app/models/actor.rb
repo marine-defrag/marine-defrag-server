f2743c3f3643e0fbe903ad86edf6e4a02fe4416a
Philip Arndt <git@p.arndt.io>
2021-11-09 10:29:12 +1300

    Allow actors' code column to be nullable

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,5 @@
 class Actor < VersionedRecord
   belongs_to :actortype, required: true
 
-  validates :code, presence: true
   validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_064922) do
+ActiveRecord::Schema.define(version: 2021_11_08_212159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_064922) do
 
   create_table "actors", force: :cascade do |t|
     t.bigint "actortype_id", null: false
-    t.string "code", null: false
+    t.string "code"
     t.string "title", null: false
     t.text "description"
     t.text "activity_summary"
