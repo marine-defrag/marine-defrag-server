2465ba4f0d4046c82443788fbc52570f87bc6321
Philip Arndt <git@p.arndt.io>
2021-11-16 20:09:13 +1300

    false I mean

--- db/migrate/20211116065947_remove_null_constraints_from_created_by_id_and_updated_by_id.rb
+++ db/migrate/20211116065947_remove_null_constraints_from_created_by_id_and_updated_by_id.rb
@@ -7,8 +7,8 @@ class RemoveNullConstraintsFromCreatedByIdAndUpdatedById < ActiveRecord::Migrati
   end
 
   def down
-    change_column :actor_measures, :created_by_id, :bigint, null: true
-    change_column :measure_actors, :created_by_id, :bigint, null: true
+    change_column :actor_measures, :created_by_id, :bigint, null: false
+    change_column :measure_actors, :created_by_id, :bigint, null: false
     change_column :actor_measures, :updated_by_id, :bigint, null: false
     change_column :measure_actors, :updated_by_id, :bigint, null: false
   end
