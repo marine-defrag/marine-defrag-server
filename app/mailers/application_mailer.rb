23e92eea5282736e1cabd1c497963b956c26801d
tmfrnz <timo@dumpark.com>
2024-11-27 21:10:55 +0100

    replace with string (2)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,8 +2,8 @@
 
 class ApplicationMailer < ActionMailer::Base
   default(
-    from: ENV["SMTP_FROM_NAME"] + "<" + ENV["SMTP_FROM"] + ">",
-    reply_to: ENV["SMTP_FROM_NAME"] + "<" + ENV["SMTP_FROM"] + ">",
+    from: "Marine Litter Action Mapping Tool <support@marine-defrag.org>",
+    reply_to: "Marine Litter Action Mapping Tool <support@marine-defrag.org>",
   )
   layout "mailer"
 end
