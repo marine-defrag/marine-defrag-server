2bc20517a63088b482e45cf38b395cc7e51883b8
tmfrnz <timo@dumpark.com>
2024-11-27 21:40:05 +0100

    also update feedback mailer

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -10,6 +10,9 @@ class FeedbackMailer < ApplicationMailer
     @feedback = feedback
     @user_name = feedback.user.name
 
-    mail to: ENV.fetch("FEEDBACK_EMAIL_ADDRESS"), subject: feedback.subject + " " + I18n.t("feedback_mailer.created.subject"), from: feedback.user.email
+    mail(
+      to: ENV.fetch("FEEDBACK_EMAIL_ADDRESS"),
+      subject: feedback.subject + " " + I18n.t("feedback_mailer.created.subject") + " from " + @user_name + " (" + feedback.user.email + ")",
+      reply_to: feedback.user.email)
   end
 end
