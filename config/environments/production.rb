e72d86cf55ba220c7457471f94b11f47baee1981
tmfrnz <timo@dumpark.com>
2024-11-27 18:53:48 +0100

    update SMTP config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,28 +69,27 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Production MailGun SMTP config
-  if ENV.fetch("EMAIL_ENABLED", false)
-    config.action_mailer.perform_deliveries = false
-    config.action_mailer.raise_delivery_errors = false
-  else
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      port: ENV["MAILGUN_SMTP_PORT"],
-      address: ENV["MAILGUN_SMTP_SERVER"],
-      user_name: ENV["MAILGUN_SMTP_LOGIN"],
-      password: ENV["MAILGUN_SMTP_PASSWORD"],
-      domain: "impactoss.org",
-      authentication: :plain
-    }
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default_url_options = {
-      host: ENV["ACTION_MAILER_HOST"] || "impactoss.org",
-      protocol: ENV["ACTION_MAILER_PROTOCOL"] || "https"
-    }
-    config.action_mailer.asset_host = ENV["ACTION_MAILER_ASSET_HOST"] || "https://impactoss.org"
-  end
+  # Production SMTP config
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: ENV["SMTP_PORT"],
+    address: ENV["SMTP_SERVER"],
+    user_name: ENV["SMTP_LOGIN"],
+    password: ENV["SMTP_PASSWORD"],
+    domain: ENV["SMTP_DOMAIN"],
+    authentication: :plain,
+    enable_starttls_auto: true,
+    openssl_verify_mode: "none",
+    ssl: true,
+    tls: true
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {
+    host: ENV["ACTION_MAILER_HOST"],
+    protocol: ENV["ACTION_MAILER_PROTOCOL"] || "https"
+  }
+  config.action_mailer.asset_host = ENV["ACTION_MAILER_ASSET_HOST"]
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
