cd706deb036e08796d16e7844e663db90fb68871
tmfrnz <timo@dumpark.com>
2024-11-27 20:11:50 +0100

    try and add name to from address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV["SMTP_FROM"]
+  default(
+    from: ENV["SMTP_FROM_NAME"] + "<" + ENV["SMTP_FROM"] + ">",
+    reply_to: ENV["SMTP_FROM_NAME"] + "<" + ENV["SMTP_FROM"] + ">",
+  )
   layout "mailer"
 end
