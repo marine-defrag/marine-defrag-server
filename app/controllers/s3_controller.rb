dde72fc062f33eb6fa888283ab08faece7c6df1e
tmfrnz <timo@dumpark.com>
2025-07-15 14:51:58 +0200

    Prevents browser execution

--- app/controllers/s3_controller.rb
+++ app/controllers/s3_controller.rb
@@ -13,7 +13,11 @@ class S3Controller < ApplicationController
     end
 
     options = {path_style: true}
-    headers = {"Content-Type" => params[:contentType], "x-amz-acl" => "public-read"}
+    headers = {
+      "Content-Type" => params[:contentType],
+      "x-amz-acl" => "public-read",
+      "Content-Disposition" => "attachment" 
+    }
 
     object_path = "#{ENV["S3_ASSET_FOLDER"]}/#{params[:objectName]}"
     s3_url = ::FogStorage.put_object_url(ENV["S3_BUCKET_NAME"], object_path, 15.minutes.from_now.to_time.to_i, headers, options)
