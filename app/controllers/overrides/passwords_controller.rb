06580a87cb4eb4c3f9ef1583a2f0f8c539744c5a
tmfrnz <timo@dumpark.com>
2025-07-21 12:55:41 +0200

    allow redirect only to CLIENT URL

--- app/controllers/overrides/passwords_controller.rb
+++ app/controllers/overrides/passwords_controller.rb
@@ -1,6 +1,18 @@
 class Overrides::PasswordsController < DeviseTokenAuth::PasswordsController
   def edit
-    # TEMP FIX: Allow external redirects
-    redirect_to params[:redirect_url], allow_other_host: true
+    redirect_url = params[:redirect_url]
+
+    begin
+      trusted_host = URI.parse(ENV["CLIENT_URL"].to_s.strip).host
+      uri = URI.parse(redirect_url)
+
+      if uri.host == trusted_host
+        redirect_to redirect_url, allow_other_host: true
+      else
+        render plain: "Unsafe redirect", status: :forbidden
+      end
+    rescue URI::InvalidURIError
+      render plain: "Invalid redirect URL", status: :bad_request
+    end
   end
 end
