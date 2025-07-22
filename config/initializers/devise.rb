f92da388032d78693c447c53c2203a9fa3c56631
tmfrnz <timo@dumpark.com>
2025-07-22 15:49:26 +0200

    fix warden in sessions controller (9)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -183,7 +183,7 @@ Devise.setup do |config|
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  config.maximum_attempts = 5
+  config.maximum_attempts = 2
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   config.unlock_in = 3.minutes
@@ -256,9 +256,9 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
-  config.warden do |manager|
-    manager.failure_app = CustomFailure
-  end
+  # config.warden do |manager|
+  #   manager.failure_app = CustomFailure
+  # end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
